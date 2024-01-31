b204965623892dceab2033c2fa0166ced9ff2306
Araf020 <araf.reputation1999@gmail.com>
2024-02-01 01:26:58 +0600

    Filter class added for searching

---
+++ src/arafat/filter/BaseFilter.java
@@ -0,0 +1,12 @@
+package src.arafat.filter;
+
+public class BaseFilter {
+    
+    public boolean searchByAccountNumber;
+    public boolean searchByAccountName;
+    public boolean searchByAccountType;
+    public boolean searchByBalance;
+    public boolean searchByOpeningDate;
+    public boolean searchByPhoneNumber;
+
+}
