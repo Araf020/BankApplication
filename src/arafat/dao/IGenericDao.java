0c0d34aa03a2a9303f07dc3add9cccd6e80fbe7c
Araf020 <araf.reputation1999@gmail.com>
2024-02-01 01:28:41 +0600

    a data access object interface added

---
+++ src/arafat/dao/IGenericDao.java
@@ -0,0 +1,12 @@
+package src.arafat.dao;
+
+import java.util.Map;
+
+public interface IGenericDao<T> {
+    
+    T insert(Long id, T t);
+    T update(Long id, T t);
+    void delete(Long id);
+    T get(Long id);
+    Map<Long, T> getAll();
+}
